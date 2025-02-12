ec28470f07411b70fe6a5c905ebd25b383027b3d
Michael Goodyear <m@gdyr.co>
2025-02-13 02:00:37 +1100

    Switch RSSI to simple match

--- src/lib/util.rs
+++ src/lib/util.rs
@@ -1,41 +1,44 @@
-pub fn hex_to_rssi(hex: u8) -> i8 {
-
-    // Handle out-of-range values
-    if hex > 0xC0 {
-        return -92
-    } else if hex < 0x0B {
-        return -44
+pub fn hex_to_rssi(hex_value: u8) -> i8 {
+    match hex_value {
+        0xC0..=0xFF => -92,
+        0xBC..=0xBF => -91,
+        0xBB => -90,
+        0xB9..=0xBA => -89,
+        0xB8 => -88,
+        0xAE..=0xB7 => -87,
+        0xA9..=0xAD => -86,
+        0xA2..=0xA8 => -85,
+        0x92..=0xA1 => -84,
+        0x8D..=0x91 => -83,
+        0x86..=0x8C => -82,
+        0x82..=0x85 => -81,
+        0x7D..=0x81 => -80,
+        0x79..=0x7C => -79,
+        0x75..=0x78 => -78,
+        0x72..=0x74 => -77,
+        0x6F..=0x71 => -76,
+        0x6B..=0x6E => -75,
+        0x68..=0x6A => -74,
+        0x66..=0x67 => -73,
+        0x63..=0x65 => -72,
+        0x5F..=0x62 => -71,
+        0x5B..=0x5E => -70,
+        0x58..=0x5A => -69,
+        0x54..=0x57 => -68,
+        0x4F..=0x53 => -67,
+        0x4B..=0x4E => -66,
+        0x47..=0x4A => -65,
+        0x43..=0x46 => -64,
+        0x3D..=0x42 => -63,
+        0x2A..=0x3C => -62,
+        0x25..=0x29 => -60,
+        0x1A..=0x24 => -58,
+        0x16..=0x19 => -56,
+        0x13..=0x15 => -54,
+        0x11..=0x12 => -52,
+        0x0E..=0x10 => -50,
+        0x0D => -48,
+        0x0C => -46,
+        0x00..=0x0B => -44,
     }
-
-    let lookup_table: [(u8, i8); 40] = [
-        (0xC0, -92), (0xBC, -91), (0xBB, -90), (0xB9, -89), (0xB8, -88),
-        (0xAE, -87), (0xA9, -86), (0xA2, -85), (0x92, -84), (0x8D, -83),
-        (0x86, -82), (0x82, -81), (0x7D, -80), (0x79, -79), (0x75, -78),
-        (0x72, -77), (0x6F, -76), (0x6B, -75), (0x68, -74), (0x66, -73),
-        (0x63, -72), (0x5F, -71), (0x5B, -70), (0x58, -69), (0x54, -68),
-        (0x4F, -67), (0x4B, -66), (0x47, -65), (0x43, -64), (0x3D, -63),
-        (0x2A, -62), (0x25, -60), (0x1A, -58), (0x16, -56), (0x13, -54),
-        (0x11, -52), (0x0E, -50), (0x0D, -48), (0x0C, -46), (0x0B, -44),
-    ];
-
-    // Handle exact matches
-    #[allow(clippy::explicit_iter_loop)]
-    if let Some((_, rssi)) = lookup_table.iter().find(|&&(h, _)| h == hex) {
-        return *rssi;
-    }
-
-    // Find the nearest match
-    let mut min_diff = i16::MAX;
-    let mut rssi = 0;
-    #[allow(clippy::explicit_iter_loop)]
-    for &(h, r) in lookup_table.iter() {
-        let diff = (i16::from(h) - i16::from(hex)).abs();
-        if diff < min_diff {
-            min_diff = diff;
-            rssi = r;
-        }
-    }
-
-    rssi
-
 }
